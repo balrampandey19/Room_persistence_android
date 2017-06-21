5d85619a8a55b11d2b476f430c669f6087a53920
Balram Pandey <balrampandey19@gmail.com>
2017-06-21 13:03:04 +0530

    add adapter kotlin

---
+++ app/src/main/java/balram/com/room_persistence/ListAdapter.kt
@@ -0,0 +1,34 @@
+package balram.com.room_persistence
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+
+/**
+ * Created by redcarpet on 6/20/17.
+ */
+
+class ListAdapter : ArrayAdapter<Person> {
+
+    constructor(context: Context, textViewResourceId: Int) : super(context, textViewResourceId) {}
+
+    constructor(context: Context, resource: Int, items: List<Person>) : super(context, resource, items) {}
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var v = convertView as View
+
+        if (v == null) {
+            val vi: LayoutInflater
+            vi = LayoutInflater.from(context)
+            v = vi.inflate(R.layout.itemlistrow, null)
+        }
+
+        return v
+
+    }
+
+
+}
--- app/src/main/java/balram/com/room_persistence/MainActivity.kt
+++ app/src/main/java/balram/com/room_persistence/MainActivity.kt
@@ -2,11 +2,43 @@ package balram.com.room_persistence
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.widget.ListView
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+    var list: List<Person> = ArrayList<Person>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var listview = findViewById(R.id.recyclerview) as ListView
+        var flotingButton = findViewById(R.id.myFAB) as FloatingActionButton
+    }
+
+    fun addPerson(firstName: String, lastName: String) {
+        val person = Person(0, firstName, lastName)
+
+        Single.fromCallable {
+            MyApp.database?.personDao()?.insert(person)
+        }.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()).subscribe()
+    }
+    fun registerAllPersonListener() {
+
+        MyApp.database?.personDao()?.getAllPeople()
+                ?.subscribeOn(Schedulers.io())
+                ?.observeOn(AndroidSchedulers.mainThread())
+                ?.subscribe { listOfPeople ->
+
+                }
+    }
+
+    fun populateListView()
+    {
+
+
     }
 }
